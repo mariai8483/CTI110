30f95038c4ac267d5fd3376f20662d9680a1cf6f
mariai8483 <mariai8483@student.faytechcc.edu>
2026-03-16 16:26:22 -0400

    Add files via upload
    second assignment

---
+++ P1HW1_MariaIvana.py
@@ -0,0 +1,25 @@
+#Ivana Maria 
+#3/1/2026
+#P1HW1_MariaIvana
+#Mathematical expressions
+
+print("----Calculating Exponenets----")
+
+base = int(input ("Enter an integar as the base value: "))
+exponent = int(input ("Enter an integar as the Exponent: "))
+
+power = base ** exponent 
+
+
+print(f"{base} raised to the power of {exponent} is {power} !!")
+
+print("---Addition and Subtraction---")
+
+start = int(input("Enter a starting integar: "))
+add_num = int(input("Enter an Integar to add: "))
+sub_num = int(input("Enter an integar to subtract: "))
+
+result = start + add_num - sub_num 
+
+print(f"{start} + {add_num} - {sub_num} is equal to {result}")
+
